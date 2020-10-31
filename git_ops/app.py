4f305f26d2a638f1992acbd3d3679d24fd17e47c
zapp-oz <shresthdewan@gmail.com>
2020-10-31 20:03:50 +0530

    Add classes and re-structure

--- git_ops/app.py
+++ git_ops/app.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import sys
 import shutil
+from tkinter import filedialog
 
 def getargs():
     #initialize the parser
@@ -20,38 +21,54 @@ def getargs():
     #return the parsed args
     return parser.parse_args()
 
+def getdir():
+
+    input_dir = filedialog.askdirectory(
+        title='new repo location'
+    )
+    return input_dir
+
 def main():
     args = getargs()
+    local_repo_path = getdir()
 
-    cwd = os.getcwd()
-    repo_path = os.path.join(cwd, '../' + args.output_repo_name)#fix this
-    repo = git_ops.clone(args.input_repo, repo_path, args.username, args.password)
-
-    git_ops.delete_history(
-        repo_path,
-        repo, 
+    #setting up local repo for the repo to be cloned
+    local_git_repo = git_ops.Git_Ops(
         args.username,
+        args.password,
+        local_repo_path,
         args.output_repo_name
     )
 
-    status = git_ops.new_github_repo(
-        args.username, 
-        args.password, 
-        args.output_repo_name
+    local_git_repo.clone(args.input_repo)
+
+    local_git_repo.reset_git()
+
+    status = local_git_repo.set_new_remote(
+        'origin',
+        'https://github.com/' + args.username + '/' + args.output_repo_name
     )
+
+    if not status:
+        shutil.rmtree(os.path.join(local_repo_path, args.output_repo_name))
+
+
+    #pushing reset repo to github
+    remote_github_repo = git_ops.Github_Ops(args.username, args.password)
+
+    status = remote_github_repo.new_github_repo(args.output_repo_name)
 
     if not status:
         shutil.rmtree(repo_path)
         sys.exit('Error creating github repo')
 
-    status = git_ops.push_repo(
-        repo, 
-        args.username, 
-        args.password,
-        args.output_repo_name
+    status = local_git_repo.push_repo(
+        'refs/heads/main',
+        'origin'
     )
 
     if not status:
+        remote_github_repo.delete_github_repo(args.output_repo_name)
         shutil.rmtree(repo_path)
         sys.exit('Error pushing the code to gihub')
 
---
+++ git_ops/git_ops.py
@@ -0,0 +1,173 @@
+import os
+import pygit2
+import json
+import requests
+
+class Github_Ops:
+
+    def __init__(self, username, password):
+        
+        self._username = username
+        self._password = password
+
+    def new_github_repo(self, repo_name):
+        
+        response = requests.post(
+            url = 'https://api.github.com/user/repos', 
+            headers = {
+                'Accept': 'application/vnd.github.v3+json'
+            }, 
+            data = json.dumps({
+                'name': repo_name
+            }), 
+            auth = (self._username, self._password)
+        )
+        
+        if response.status_code == 201:
+            return True
+        else:
+            return False
+
+    def delete_github_repo(self, repo_name):
+
+        response = requests.delete(
+                url = 'https://api.github.com/repos/' + self._username + '/' + repo_name,
+                headers = {
+                'Accept': 'application/vnd.github.v3+json'
+                },
+                auth = (self._username, self._password)
+            )
+
+        if response.status_code == 204:
+            return True
+        else: 
+            return False
+    
+
+
+class Git_Ops:
+
+    def __init__(self, username, password, local_repo_path, local_repo_name):
+        self._username = username
+        self._password = password
+        self._local_repo_path = local_repo_path
+        self._local_repo_name = local_repo_name
+
+    #clone git repo and initialize submodules
+    def clone(self, github_repo_url):
+
+        cloned_repo = pygit2.clone_repository(
+            github_repo_url, 
+            os.path.join(self._local_repo_path, self._local_repo_name),
+            callbacks = pygit2.RemoteCallbacks(
+                    pygit2.UserPass(
+                        self._username, 
+                        self._password
+                    )
+                )
+        )
+
+        cloned_repo.init_submodules()
+
+        cloned_repo.update_submodules()
+
+
+    def create_commit(self, ref, commit_message):
+
+        #get the local git repo
+        repo = pygit2.Repository(
+            os.path.join(self._local_repo_path, self._local_repo_name)
+        )
+
+        #add all files to staging area
+        index = repo.index
+        index.add_all()
+        index.write()
+
+        #create a new tree empty tree object
+        author = pygit2.Signature(
+            self._username + ' Author', 
+            self._username + '@authors.tld'
+        )
+        
+        committer = pygit2.Signature(
+            self._username + ' Committer', 
+            self._username + '@committers.tld'
+        )
+        
+        tree = index.write_tree()
+
+        #create a new commit in a new branch
+        repo.create_commit(
+            ref,
+            author,
+            committer,
+            commit_message,
+            tree,
+            []
+        )
+
+        #change the HEAD to new branch
+        repo.checkout(ref)
+
+    def set_new_remote(self, remote_name, remote_url):
+        try:
+            repo.remotes.create(
+                remote_name, 
+                remote_url
+            )
+            return True
+            
+        except Exception:
+            return False
+
+    def push_repo(self, ref, remote_name):
+        try:
+            repo.remotes[remote_name].push(
+                [ref],
+                pygit2.RemoteCallbacks(
+                    pygit2.UserPass(
+                        self._username, 
+                        self._password
+                    )
+                )
+            )
+            return True
+
+        except Exception:
+            return False
+    
+
+    #delete all the git repo history
+    def reset_git(self):
+
+        local_repo_path = os.path.join(self._local_repo_path, self._local_repo_name)
+
+        #get the local git repo
+        repo = pygit2.Repository(
+            local_repo_path
+        )
+
+        #delete README.md
+        if os.path.exists(os.path.join(local_repo_path, 'README.md')):
+            os.remove(os.path.join(local_repo_path, 'README.md'))
+
+        #delete License.txt
+        if(os.path.exists(os.path.join(local_repo_path, 'LICENSE'))):
+            os.remove(os.path.join(local_repo_path, 'LICENSE'))
+
+        self.create_commit('refs/heads/new', 'Init Commit')
+
+        #delete all other branches
+        for b in repo.branches.local:
+            if(b != 'new'):
+                repo.branches.delete(b)
+
+        #rename new branch to main
+        repo.branches['new'].rename('main')
+
+        #delete all remotes
+        n = len(repo.remotes)-1
+        for r in range(0, len(repo.remotes)):
+            repo.remotes.delete(repo.remotes[n].name)
+            n -= 1
